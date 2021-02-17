76e808c21b7f1c0f5ea4d83754febf483494f8d4
Isabel <isabel.asenjo@correo.unimet.edu.ve>
2021-02-17 16:19:07 -0400

    Corrección

--- e2.py
+++ e2.py
@@ -20,6 +20,7 @@ while True:
     while not action.isnumeric() or int(action) not in range(1,5):
         action = input("Ingreso inválido, ingrese el número correspondiente a su elección: ")
 
+    print("\n")
     if action == "1":
         # datos
         name = input("Nombre del cliente: ").title()
@@ -43,11 +44,13 @@ while True:
             discount = '40%'
             print(f"Por tener más de 55 años, tiene un descuento del 40% del precio del boleto. Su precio final es de {price}")
         if (int(id[-1]) > int(id[-2])) and (int(id[-2]) > int(id[-3])):
-            price -= (price*0.30)
             if price == 40:
                 discount = '30%'
             else:
                 discount = '40% & 30%'
+            
+            price -= (price*0.30)
+            
             print(f"Por tener como tres últimos dígitos de su cédula un número incremental, tiene un descuento del 30% del precio del boleto. Su precio final es de {price}")
         
         clients.append({'name': name, 'age': age, 'id': id, 'price': price, 'discount': discount})
@@ -55,7 +58,7 @@ while True:
         print(f"Cliente registrado con éxito!\n\tNombre: {name}\n\tEdad: {age}\n\tCédula: {id}\n\tPrecio del boleto: ${price}\n\tDescuento recibido: {discount}")
 
     elif action == "2":
-        print("\nCLIENTES REGISTRADOS\n")
+        print("CLIENTES REGISTRADOS\n")
         for i,c in enumerate(clients):
             print(f"----{i+1}----")
             print(f"\tNombre: {c['name']}\n\tEdad: {c['age']}\n\tCédula: {c['id']}\n\tPrecio del boleto: ${c['price']}\n\tDescuento recibido: {c['discount']}")
@@ -68,13 +71,13 @@ while True:
                 print(f"El cliente {eliminado['name']} fue eliminado con éxito.")
     
     elif action == "3":
-        print("\nCLIENTES REGISTRADOS\n")
+        print("CLIENTES REGISTRADOS\n")
         for i,c in enumerate(clients):
             print(f"----{i+1}----")
             print(f"\tNombre: {c['name']}\n\tEdad: {c['age']}\n\tCédula: {c['id']}\n\tPrecio del boleto: ${c['price']}\n\tDescuento recibido: {c['discount']}")
 
     else:
-        print("\nHasta pronto!")
+        print("Hasta pronto!")
         break
 
     print("\n")
